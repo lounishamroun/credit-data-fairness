39c87610fb24d16566f40f1d6a77f73e63d1eaa6
AMATERASU11 <55045153+AMATERASU11@users.noreply.github.com>
2026-04-06 13:30:51 +0200

    adding notebook

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,18 +1,19 @@
 from ucimlrepo import fetch_ucirepo 
 import json
+import pandas as pd
   
 '''Fetch Dataset'''
 
 statlog_german_credit_data = fetch_ucirepo(id=144)  
 X = statlog_german_credit_data.data.features.copy()
 y = statlog_german_credit_data.data.targets.copy()
-X.to_csv('data/features.csv',Index=False)
-y.to_csv('data/labels.csv',Index=False)
+X.to_csv('data/features.csv', index=False)
+y.to_csv('data/labels.csv', index=False)
 
 '''Feature Engineering'''
 
 # Load a JSON schema containing metadata
-with open("data_schema.json", "r", encoding="utf-8") as f:
+with open("data_metdata.json", "r", encoding="utf-8") as f:
     schema = json.load(f)
 
 # Get feature groups from schema
